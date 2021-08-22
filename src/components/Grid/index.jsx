38021d8405df12e775d05783c9e0c3a0d821facb
Eduardo Picolo <eduardonpicolo@gmail.com>
2021-08-22 15:23:13 -0300

    Criado Grid
    Co-authored-by: Sayuck <nobrega.m.roberto@gmail.com>

---
+++ src/components/Grid/index.jsx
@@ -0,0 +1,47 @@
+import { useEffect } from 'react';
+import Node from '../Node';
+import * as S from './styles';
+
+function Grid() {
+  class Cell {
+    constructor(row, col) {
+      this.row = row;
+      this.col = col;
+      this.isStart = row === START_NODE.row && col === START_NODE.col;
+      this.isEnd = row === END_NODE.row && col === END_NODE.col;
+      this.isVisited = false;
+      this.isWall = false;
+      this.distance = Infinity;
+      this.previousNode = null;
+    }
+  }
+
+  useEffect(() => {
+    const table = Array.from({ length: NUMBER_OF_ROWS }, () => []);
+    for (let row = 0; row < NUMBER_OF_ROWS; row++) {
+      for (let col = 0; col < NUMBER_OF_COLS; col++) {
+        const newNode = new Cell(row, col);
+        table[row].push(newNode);
+        if (newNode.isStart) setStartNode(newNode);
+        if (newNode.isEnd) setEndNode(newNode);
+      }
+    }
+    setGrid?.(table);
+  }, []);
+
+  return (
+    <S.Grid>
+      <tbody>
+        {grid?.map((currentRow, rowIdx) => (
+          <tr key={rowIdx} id={`row-${rowIdx}`}>
+            {currentRow.map((cell, cellIdx) => {
+              return <Node key={cellIdx} {...cell} />;
+            })}
+          </tr>
+        ))}
+      </tbody>
+    </S.Grid>
+  );
+}
+
+export default Grid;
---
+++ src/components/Grid/styles.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Grid = styled.table`
+  border-collapse: collapse;
+  white-space: normal;
+  line-height: normal;
+  font-weight: normal;
+  font-size: medium;
+  font-style: normal;
+  color: -internal-quirk-inherit;
+  text-align: start;
+  border-spacing: 0px;
+  border: 0px;
+  font-variant: normal;
+  margin: 0 auto;
+  max-width: inherit;
+
+  tr {
+    display: table-row;
+    vertical-align: inherit;
+    border-color: inherit;
+  }
+`;
