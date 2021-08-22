dfc0681df9b3fbb03786ab0db6d87bbbc651ad6b
Eduardo Picolo <eduardonpicolo@gmail.com>
2021-08-22 15:36:28 -0300

    Implementado Home page
    Co-authored-by: Sayuck <nobrega.m.roberto@gmail.com>

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import Grid from '../components/Grid';
+import Header from '../components/Header';
+import Instructions from '../components/Instructions';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Instructions />
+      <Grid />
+    </>
+  );
+}
